74b0e6b89d7c34502ad824b6923cebbb484b6d4d
L0H4NX <lsteferson83@gmail.com>
2022-08-22 16:09:04 -0300

    Fist commit

---
+++ src/main/java/com/loha/boockstore/BoockstoeApplication.java
@@ -0,0 +1,13 @@
+package com.loha.boockstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoockstoeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoockstoeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loha/boockstore/BoockstoeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loha.boockstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoockstoeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
